4df7b5897d8f79b799ce05fb80660b2b54b3ed12
maxlv7 <admin@maxlv.org>
2019-10-30 13:50:56 +0800

    解决数据库超时连接失败问题

--- app/util/db.py
+++ app/util/db.py
@@ -1,25 +1,18 @@
-import pymysql.cursors
+from pymysql.connections import Connection
+from pymysql.cursors import DictCursor
 
 from config import db_config
-connection = pymysql.connect(**db_config,cursorclass=pymysql.cursors.DictCursor)
 
-# try:
-#     with connection.cursor() as cursor:
-#         # Create a new record
-#         sql = "INSERT INTO message(message_type,content,qq,wechat,status)  VALUES ('10001','content','qq','wechat','1') "
-#         r = cursor.execute(sql)
-#         print(r)
-#
-#     # connection is not autocommit by default. So you must commit to save
-#     # your changes.
-#     connection.commit()
-#
-#     # with connection.cursor() as cursor:
-#     #     # Read a single record
-#     #     sql = "SELECT message.qq,type_name FROM `message`,message_type WHERE message.message_type=message_type.message_type"
-#     #     cursor.execute(sql)
-#     #     result = cursor.fetchall()
-#     #     print(result)
-# finally:
-#     connection.close()
-# print()
+
+class my_connection(Connection):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def cursor(self, cursor=None):
+        if cursor:
+            return cursor(self)
+        self.ping()
+        return self.cursorclass(self)
+
+
+connection = my_connection(**db_config, cursorclass=DictCursor)
